300fe604bf08d4eff96a6dc3422b79ba12be5717
“Rasik <jainrasik@gmail.com>
2018-11-15 12:18:50 +0530

    DB changes

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const sql = require('mssql');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const config = {
+  user: '...',
+  password: '...',
+  server: 'localhost', // You can use 'localhost\\instance' to connect to named instance
+  database: '...',
+
+  options: {
+    encrypt: true // Use this if you're on Windows Azure
+  }
+};
+
+sql.connect(config).catch((err) => {
+  debug(err);
+});
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 
